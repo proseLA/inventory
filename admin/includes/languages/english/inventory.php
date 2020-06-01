9b262f6f9c9b21884ca24e378303f661d99c29be
pRose on spring <prose@rossroberts.com>
2020-06-01 07:00:53 -0700

    initial commit

---
+++ admin/includes/languages/english/inventory.php
@@ -0,0 +1,34 @@
+<?php
+
+
+define('TABLE_HEADING_ID', 'ID');
+define('TABLE_HEADING_MODEL', 'Model');
+define('TABLE_HEADING_NAME', 'Product Name');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_QTY', 'Quantity');
+define('TABLE_HEADING_PRICE', 'Price');
+
+define('PRODUCTS_UPDATED', ' product(s) inventory updated.');
+
+define('TABLE_HEADING_CUSTOMER_NOTIFIED', 'Customer Notified');
+define('TABLE_HEADING_DATE_ADDED', 'Date Added');
+define('TABLE_HEADING_STATUS', 'Status');
+
+define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_TAX', 'Tax');
+define('TABLE_HEADING_TOTAL', 'Total');
+define('TABLE_HEADING_PRICE_EXCLUDING_TAX', 'Price (ex)');
+define('TABLE_HEADING_PRICE_INCLUDING_TAX', 'Price (inc)');
+define('TABLE_HEADING_TOTAL_EXCLUDING_TAX', 'Total (ex)');
+define('TABLE_HEADING_TOTAL_INCLUDING_TAX', 'Total (inc)');
+define('TABLE_HEADING_PRICE', 'Price');
+define('ENTRY_CUSTOMER', 'CUSTOMER:');
+
+define('ENTRY_SOLD_TO', 'SOLD TO:');
+define('ENTRY_SHIP_TO', 'SHIP TO:');
+define('ENTRY_PAYMENT_METHOD', 'Payment Method:');
+define('ENTRY_SHIPPING', 'Shipping:');
+define('ENTRY_DATE_PURCHASED', 'Date Ordered:');
+
+define('ENTRY_ORDER_ID','Order No. ');
+define('TEXT_INFO_ATTRIBUTE_FREE', '&nbsp;-&nbsp;FREE');
